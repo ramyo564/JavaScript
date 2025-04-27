15cbc36e9c53508caaa1d99865430c610c1eb2c5
ramyo564 <yohan032yohan@gmail.com>
2025-04-27 14:40:44 +0900

    컴포넌트 단위로 모듈화

---
+++ TEST/src/App.js
@@ -0,0 +1,9 @@
+import TapBar from "./componets/TabBar";
+import Content from "./componets/Content";
+
+export default function App($app){
+
+    const tabBar = new TapBar();
+    const content = new Content();
+
+}
