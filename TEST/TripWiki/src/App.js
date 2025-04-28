4e3cdb0c2291b316aed2075e9e83b666c174384c
ramyo564 <yohan032yohan@gmail.com>
2025-04-28 22:31:07 +0900

    TripWiki -> App.js 기본구성

--- TEST/TripWiki/src/App.js
+++ TEST/TripWiki/src/App.js
@@ -1,3 +1,30 @@
-export default function App(){
-    
+import Header from './components/Header.js';
+import RegionList from './components/RegionList.js';
+import CityList from './components/CityList.js';
+import CityDetail from './components/CityDetail.js';
+import CityList from './components/CityList.js';
+
+export default function App() {
+    this.state = {
+        startIdx: 0,
+        sortBy: '',
+        searchWord: '',
+        region: '',
+        cities: '',
+
+    };
+
+    const header = new Header();
+    const regionList = new RegionList();
+    const cityList = new CityList();
+    const cityDetail = new CityDetail();
+
+    this.setState = (newState) => {
+        this.state = newState;
+    };
+
+    const init = () => {};
+
+    init();
+
 }
