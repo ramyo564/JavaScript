d5d135f80cebd2038326424d548733b7d9558500
ramyo564 <yohan032yohan@gmail.com>
2025-04-29 00:16:33 +0900

    TripWiki -> CityList 컴포넌트 추가

--- TEST/TripWiki/src/App.js
+++ TEST/TripWiki/src/App.js
@@ -2,9 +2,10 @@ import Header from './components/Header.js';
 import RegionList from './components/RegionList.js';
 import CityList from './components/CityList.js';
 import CityDetail from './components/CityDetail.js';
-import CityList from './components/CityList.js';
 
-export default function App() {
+import { request } from './components/api.js';
+
+export default function App($app) {
     this.state = {
         startIdx: 0,
         sortBy: '',
@@ -16,14 +17,26 @@ export default function App() {
 
     const header = new Header();
     const regionList = new RegionList();
-    const cityList = new CityList();
+    const cityList = new CityList({ $app, initialState: this.state.cities });
     const cityDetail = new CityDetail();
 
     this.setState = (newState) => {
         this.state = newState;
+        cityList.setState(this.state.cities);
     };
 
-    const init = () => {};
+    const init = async () => {
+        const cities = await request(
+            this.state.startIdx,
+            this.state.region,
+            this.state.sortBy,
+            this.state.searchWord
+        )
+        this.setState({
+            ...this.state,
+            cities:cities,
+        });
+    };
 
     init();
 
--- TEST/TripWiki/src/components/CityList.js
+++ TEST/TripWiki/src/components/CityList.js
@@ -1,13 +1,28 @@
-export default function CityList(){
+export default function CityList({ $app, initialState }) {
+    this.state = initialState;
     this.$target = document.createElement('div');
     this.$target.className = 'city-list';
 
-    this.template = () => {
+    $app.appendChild(this.$target);
 
+    this.template = () => {
+        let temp = `<div class="city-items-container">`;
+        if(this.state){
+            this.state.cities.forEach(element => {
+                temp += `
+                <div class="city-item" id=${element.id}>
+                    <img src=${element.image}></img>
+                    <div class="city-item-info">${element.city},${element.country}</div>
+                    <div class="city-item-score">⭐️${element.total}</div>
+                </div>`
+            });
+            temp += `</div>`;
+        }
+        return temp;
     };
 
     this.render = () => {
-
+        this.$target.innerHTML = this.template();
     };
 
     this.setState = (newState) => {
