608d552e2e672d34c9c299b4b3467b31c2b595fb
ramyo564 <yohan032yohan@gmail.com>
2025-04-29 19:10:59 +0900

    TripWiki -> CityDetail.js -> 배너 구현

--- TEST/TripWiki/src/components/CityDetail.js
+++ TEST/TripWiki/src/components/CityDetail.js
@@ -1,13 +1,27 @@
-export default function CityDetail(){
+export default function CityDetail({ $app, initialState }) {
+    this.state = initialState;
     this.$target = document.createElement('div');
     this.$target.className = 'city-detail';
 
-    this.template = () => {
+    $app.appendChild(this.$target);
 
+    this.template = () => {
+        let cityData = this.state.cityDetail;
+        let temp = ``;
+        if (cityData) {
+            temp = `<div class="image-banner">
+                <img src="${cityData.image}" />
+                <div class="city-name">
+                    <div class="city">${cityData.city}</div>               
+                    <div class="country">${cityData.region} / ${cityData.country}</div>
+                </div>
+            </div>`;
+        }
+        return temp;
     };
 
     this.render = () => {
-
+        this.$target.innerHTML = this.template();
     };
 
     this.setState = (newState) => {
